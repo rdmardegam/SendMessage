647c29f57d82828f375afb3f937c42503114ce84
rdmardegam <rdmardegam@hotmail.com>
2023-06-20 09:21:02 -0300

    Changes circuit break

--- src/main/java/com/example/sendmessage/config/CircuitBreakConfig.java
+++ src/main/java/com/example/sendmessage/config/CircuitBreakConfig.java
@@ -1,11 +1,17 @@
 package com.example.sendmessage.config;
 
+import com.example.sendmessage.exception.BusinessException;
+import com.example.sendmessage.exception.TechnicalException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.circuitbreaker.autoconfigure.CircuitBreakerProperties;
 import io.github.resilience4j.circuitbreaker.event.CircuitBreakerOnStateTransitionEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class CircuitBreakConfig {
@@ -13,10 +19,21 @@ public class CircuitBreakConfig {
     @Autowired
     private CircuitBreakerRegistry registry;
 
+    @Autowired
+    private CircuitBreakerProperties circuitBreakerProperties;
+
     @Bean(name = "circutSendsms")
     public CircuitBreaker circuitBreakerRegistry() {
-        CircuitBreaker circuitBreaker = registry.circuitBreaker("sendsms");
+        CircuitBreakerConfig existingConfig = registry.getDefaultConfig();
+
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.from(existingConfig)
+                // Verifica se no stacktrace da exception contem a Exception TechnicalException
+                .recordException(throwable -> hasThrowableInHierarchy(throwable, TechnicalException.class))
+                .build();
+
+        CircuitBreaker circuitBreaker = registry.circuitBreaker("sendsms",circuitBreakerConfig);
         circuitBreaker.getEventPublisher().onStateTransition(this::onStateChange);
+
         return circuitBreaker;
     }
 
@@ -26,5 +43,17 @@ public class CircuitBreakConfig {
         String toState = event.getStateTransition().getToState().name();
 
         System.out.println("CircuitBreaker state changed from " + fromState + " to " + toState);
+    }
+
+    private boolean hasThrowableInHierarchy(Throwable throwable, Class<? extends Throwable> targetExceptionClass) {
+        if (throwable == null || targetExceptionClass == null) {
+            return false;
+        }
+
+        if (targetExceptionClass.isInstance(throwable)) {
+            return true;
+        }
+
+        return hasThrowableInHierarchy(throwable.getCause(), targetExceptionClass);
     }
 }
